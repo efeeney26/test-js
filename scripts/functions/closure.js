2380f0bf8f9515c36bf2cc1abbf631c7998f5fe1
Kochkin Danila <>
2020-11-30 10:24:04 +0300

    closure

--- scripts/functions/closure.js
+++ scripts/functions/closure.js
@@ -1,6 +1,18 @@
 'use strict'
 
+const sum = (a) => (b) => console.log(a + b)
+
+const inBetween = (a, b) => (item) => item >= a && item <= b
+const isArray = (array) => (item) => array.includes(item)
+
+const byField = (field) => (a, b) => a[field] > b[field] ? 1 : -1
+
 export const showClosureResults = () => {
-  const hello = 'Hello'
-  console.log([...hello])
+  //sum(5)(-1)
+
+  //console.log(arr.filter(inBetween(3, 6)))
+  //console.log(arr.filter(isArray([3, 4, 10])))
+
+  //console.log(users.sort(byField('name')))
+  //console.log(users.sort(byField('age')))
 }

117a57637e74510507cfe664a951b30df84533cf
Kochkin Danila <>
2021-07-18 14:20:49 +0300

    production

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ const main = () => {
     document.body.appendChild(component())
 }
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!')
+}
+
 main()
